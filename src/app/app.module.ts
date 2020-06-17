ff13ff904c89075ef0d9f99d9095cb6f72418933
Imen.JDIDI <imen.jdidi23@gmail.com>
2020-06-17 14:40:31 +0200

    - Creations des composants - Ajout des formulaire d'inscription et de connexion - Ajout des modeles - Integration de Google maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './composants/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UtilisateurComponent } from './composants/utilisateur/utilisateur.component';
+import { DepanneurComponent } from './composants/depanneur/depanneur.component';
+import { InscriptionComponent } from './composants/inscription/inscription.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ResultatDepanneurComponent } from './composants/resultat-depanneur/resultat-depanneur.component';
+import { AgmCoreModule} from '@agm/core'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UtilisateurComponent,
+    DepanneurComponent,
+    InscriptionComponent,
+    ResultatDepanneurComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        ReactiveFormsModule,
+        FormsModule,
+        AgmCoreModule.forRoot({
+          apiKey :'AIzaSyCfuOM2dS9S24QwiSaZWPInoSEcOYeMEU0',
+          libraries: ['places']
+        })
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/composants/home/home.component.ts
+++ src/app/composants/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  searchForm = new FormGroup({
+      codePostale : new FormControl('',[Validators.required,Validators.maxLength(5),Validators.minLength(5) , Validators.pattern(/^-?(0|[1-9]\d*)?$/)])
+  }
 
-  constructor() { }
+  );
+  selectedCategory: '';
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  rechercherDepanneur(codePostale: any, selectedCategory: any) {
+  this.router.navigate(['/resultat'],{queryParams :  {cp: codePostale, category: selectedCategory}});
+  }
+
+  selectChangeHandler(event: any) {
+    this.selectedCategory = event.target.value;
+
+  }
 }
