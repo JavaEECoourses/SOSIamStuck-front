229ad2f65d8e671fb3149f532df65d0521f37c82
Imen.JDIDI <imen.jdidi23@gmail.com>
2020-06-18 16:34:00 +0200

    - Formulaire d'inscription : amélioration et ajout des champs Catégories

--- src/app/composants/inscription/inscription.component.ts
+++ src/app/composants/inscription/inscription.component.ts
@@ -7,12 +7,24 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
-  signupForm = new FormGroup({
+    signupForm = new FormGroup({
     firstname : new FormControl('',[Validators.required])});
+    public selectedUserType: string;
+    public category :string[] = ['mécanicien','plombier','electricien'];
+    private selectedCategory: '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectChangeHandler(event: any) {
+    this.selectedUserType = event.target.value;
+
+  }
+
+  selectCategoryChangeHandler(event: any) {
+    this.selectedCategory = event.target.value;
+
+  }
 }
